34c04602ce64de061a84fd450217a907a2c8a3b9
Paul <dev.pauljoo@gmail.com>
2017-07-06 14:06:59 +1000

    Local page navigation with scroll animation. Added jQuery to build.

--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
 import * as ScrollMagic from 'scrollmagic';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
@@ -33,5 +34,37 @@ export class PageOneComponent implements OnInit {
         // var enlarge = new ScrollMagic.TweenMax(triggerElement, 2, {width:"200px", height:"150px"});
 
         var scene = new ScrollMagic.Scene().setTween(fadeToRedAnimation).triggerElement(triggerElement).addTo(controller).addIndicators();
+
+        $("a").click(function(e) {
+            var hash = window.location.hash;
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 300, "swing", function() {
+                window.location.hash = hash;
+            });
+        });
+
+//         $("a").on('click', function(event) {
+
+//     // Make sure this.hash has a value before overriding default behavior
+//     if (window.location.hash !== "") {
+//       // Prevent default anchor click behavior
+//       event.preventDefault();
+
+//       // Store hash
+//       var hash = window.location.hash;
+
+//       // Using jQuery's animate() method to add smooth page scroll
+//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+   
+//         // Add hash (#) to URL when done scrolling (default click behavior)
+//         window.location.hash = hash;
+//       });
+//     } // End if
+//   });
     }
 }
